2f3d1e39506f7272c07c153cdd20137c142c6a41
Ei Kay Khine <topnetspice@gmail.com>
2017-05-03 16:20:54 +0630

    Commit all file to features branch

---
+++ src/main/java/org/helloworld/example/jsf/ErrorCode.java
@@ -0,0 +1,19 @@
+package org.helloworld.example.jsf;
+
+public enum ErrorCode {
+
+    NOT_LOGED_IN(150),
+    SESSION_EXPIRED_MULTIPLE_LOGINS(201),
+    BAD_CREDENTIAL(300);
+
+    private int code;
+
+    private ErrorCode(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+}
